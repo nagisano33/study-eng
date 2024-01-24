f7c6d2ba5b5c8896d1d9a377fcb1cd27f7a834ae
nagisano33 <vita.stars2@gmail.com>
2024-01-25 00:42:47 +0900

    Update Registration.tsx 登録ページ実装

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,10 +1,25 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center, Stack, Text } from "@chakra-ui/react";
+import { RegistrationForm } from "./components/RegistrationForm";
 
 /**
  * 登録画面
- * 
+ *
  * @author nagisano33
  */
-export function Registration(){
-    return <Box>Registration</Box>
+export function Registration() {
+  return (
+    <Center mt={5}>
+      <Stack
+        bg={"gray.100"}
+        boxShadow={"md"}
+        rounded={"base"}
+        width={["95%", "80%", "40%"]}
+        p={10}
+        gap={10}
+      >
+        <Text fontSize="4xl">ぶんぽーますたー</Text>
+        <RegistrationForm />
+      </Stack>
+    </Center>
+  );
 }
