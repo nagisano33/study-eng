54a798839e921c6b6cc06489e67e6eefd6a1d92e
nagisano33 <vita.stars2@gmail.com>
2024-01-26 23:50:46 +0900

    Add IAnswer.ts 回答

---
+++ src/types/IAnswer.ts
@@ -0,0 +1,18 @@
+import { AnswerId } from "./AnswerId";
+
+/**
+ * 回答
+ *
+ * @author nagisano33
+ */
+export interface IAnswer {
+  /**
+   * 回答 ID
+   */
+  answerId: AnswerId;
+
+  /**
+   * テキスト
+   */
+  text: string;
+}
