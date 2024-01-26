6e4f6502dabe0e388a67ee18d9dfa3705f8c6e87
nagisano33 <vita.stars2@gmail.com>
2024-01-26 23:48:20 +0900

    Add WordClass.ts 品詞

---
+++ src/types/WordClass.ts
@@ -0,0 +1,21 @@
+/**
+ * 品詞
+ *
+ * @type {Noun} 名詞
+ * @type {Pronoun} 代名詞
+ * @type {Adjective} 形容詞
+ * @type {Verb} 動詞
+ * @type {Adverb} 副詞
+ * @type {Preposition} 前置詞
+ * @type {Conjunction} 接続詞
+ * @type {Interjection} 間投詞
+ */
+export type WordClass =
+  | "Noun"
+  | "Pronoun"
+  | "Adjective"
+  | "Verb"
+  | "Adverb"
+  | "Preposition"
+  | "Conjuction"
+  | "Interjection";
