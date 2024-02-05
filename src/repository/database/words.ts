ba7ff83a7a0e69ded9156e379eea6c982abead39
nagisano33 <vita.stars2@gmail.com>
2024-02-05 23:35:45 +0900

    Add words.ts 単語の一覧

---
+++ src/repository/database/words.ts
@@ -0,0 +1,19 @@
+import { Word } from "../../types/Word";
+
+/**
+ * 単語の定義
+ * 
+ * @author nagisano33
+ */
+export const words: Word[] = [
+  {
+    spelling: "I",
+    wordClass: "Pronoun",
+    japanese: "私",
+  },
+  {
+    spelling: "from",
+    wordClass: "Preposition",
+    japanese: "〜から",
+  },
+];
