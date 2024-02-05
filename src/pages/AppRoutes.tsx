e63ba09a3207537debffa109826bce0bdf28a32b
nagisano33 <vita.stars2@gmail.com>
2024-02-06 00:45:04 +0900

    Update AppRoutes.tsx 階層ページ遷移を仮実装

--- src/pages/AppRoutes.tsx
+++ src/pages/AppRoutes.tsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { Main } from "./main/Main";
 import { Registration } from "./registration/Registration";
+import { QuestionPage } from "./main/question/QuestionPage";
 
 /**
  * アプリメインのルーティング
- * 
+ *
  * @author nagisano33
  */
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={<Main />}>
+        <Route path="/question" element={<QuestionPage />} />
+        <Route path="/answer" element={<>answer</>} />
+      </Route>
       <Route path="/registration" element={<Registration />} />
     </Routes>
   );
