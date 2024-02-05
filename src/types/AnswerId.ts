28c8aa9911db40cf849becf88f0e386d1eba6122
nagisano33 <vita.stars2@gmail.com>
2024-02-05 23:41:19 +0900

    Update AnswerId.ts 型ガード関数を追加

--- src/types/AnswerId.ts
+++ src/types/AnswerId.ts
@@ -1,6 +1,21 @@
+const answerIds = ["A", "B", "C", "D"] as const;
+
 /**
  * 回答 ID
- * 
+ *
  * @author nagisano33
  */
-export type AnswerId = "A" | "B" | "C" | "D";
+export type AnswerId = (typeof answerIds)[number];
+
+/**
+ * AnswerId かどうかを返す
+ * @param value 任意の値
+ * @returns value が AnswerId型かどうか
+ */
+export function isAnswerId(value: unknown): value is AnswerId {
+  if (!value || typeof value !== "string") {
+    return false;
+  }
+
+  return answerIds.some((id) => id === value);
+}
