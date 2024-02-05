3b7d7e21895fa836f5f04913c1c206721be9f1f6
nagisano33 <vita.stars2@gmail.com>
2024-02-06 00:01:56 +0900

    Update Main.tsx 未登録なら登録ページへ飛ばす

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,32 @@
-import { Box } from "@chakra-ui/layout";
+import { Text } from "@chakra-ui/layout";
+import { useUserParameter } from "../../hooks/useUserParameter";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { BackgroundBox } from "../components/BackgroundBox";
+import { AnswerControl } from "./question/components/AnswerControl";
 
 /**
  * メイン画面
- * 
+ *
  * @author nagisano33
  */
-export function Main(){
-    return <Box>Main</Box>
+export function Main() {
+  const getUser = useUserParameter();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (getUser() !== null) {
+      return;
+    }
+
+    navigate("registration");
+  }, []);
+
+  return (
+    <BackgroundBox>
+      <Text>Hello {getUser()?.name}</Text>
+      <AnswerControl />
+    </BackgroundBox>
+  );
 }
