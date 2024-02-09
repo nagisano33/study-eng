9252288ccf25eb9f32db10ef0f1131a1addb7149
nagisano33 <vita.stars2@gmail.com>
2024-02-10 01:15:31 +0900

    Update useQuestionPageQuery.ts question をガード

--- src/pages/main/question/hooks/useQuestionPageQuery.ts
+++ src/pages/main/question/hooks/useQuestionPageQuery.ts
@@ -3,11 +3,16 @@ import { useQuestionPageParameter } from "./useQuestionPageParameter";
 
 /**
  * 問題ページで使用する問題を取得するカスタムクエリ
- * 
- * @author nagisano33 
+ *
+ * @author nagisano33
  */
 export function useQuestionPageQuery() {
   const { id } = useQuestionPageParameter();
 
-  return useQuestionQuery(id);
+  const { question } = useQuestionQuery(id);
+  if (!question) {
+    throw new Error("問題が見つかりません");
+  }
+
+  return { question };
 }
