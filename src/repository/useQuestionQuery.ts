ea52e7be16989617fc0941d3cc61d29427b5557e
nagisano33 <vita.stars2@gmail.com>
2024-02-10 01:14:49 +0900

    Update useQuestionQuery.ts コールバックから Ref に変更

--- src/repository/useQuestionQuery.ts
+++ src/repository/useQuestionQuery.ts
@@ -1,22 +1,29 @@
-import { useCallback } from "react";
+import { useRef } from "react";
 import { questions } from "./database/questions";
 import { Question } from "../types/Question";
 
 /**
+ * useQuestionQuery の戻り値
+ *
+ * @author nagisano33
+ */
+interface UseQuestionQueryReturn {
+  /**
+   * 問題
+   */
+  question: Question | undefined;
+}
+
+/**
  * 問題を扱うカスタムフック
+ * @param {number} questionId 問題 ID
  *
  * @autnor nagisano33
  */
-export function useQuestionQuery() {
-  /**
-   * Id で問題を取得する
-   *
-   * @param {number} questionId 問題 ID
-   * @returns 問題 または undefined
-   */
-  const get = useCallback((questionId: number): Question | undefined => {
-    return questions.find((question) => question.id === questionId);
-  }, []);
+export function useQuestionQuery(questionId: number): UseQuestionQueryReturn {
+  const question = useRef(
+    questions.find((question) => question.id === questionId)
+  );
 
-  return { get };
+  return { question: question.current };
 }
