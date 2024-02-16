fbc1d5d14e254000173f0505ba56151b1cc63d12
nagisano33 <vita.stars2@gmail.com>
2024-02-17 00:30:09 +0900

    Update App.tsx テーマを上書き

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { AppRoutes } from "./pages/AppRoutes";
 
 /**
+ * テーマ
+ */
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#f0f0f0",
+      },
+    },
+  },
+});
+
+/**
  * メインアプリ
- * 
+ *
  * @author nagisano33
  */
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AppRoutes />
     </ChakraProvider>
   );
