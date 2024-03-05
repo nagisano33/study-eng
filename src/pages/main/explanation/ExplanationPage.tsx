6e430998db68eb2ce55f1a9960956bae22dd965a
nagisano33 <vita.stars2@gmail.com>
2024-03-06 00:47:25 +0900

    Update ExplanationPage.tsx 解説ページを実装

--- src/pages/main/explanation/ExplanationPage.tsx
+++ src/pages/main/explanation/ExplanationPage.tsx
@@ -1,10 +1,36 @@
+import { Box, Button, Stack, Text } from "@chakra-ui/react";
+import { ContentBox } from "../../components/ContentBox";
+import { UserResult } from "./components/UserResult";
+import { useExplanationPageQuery } from "./hooks/useExplanationPageQuery";
+
 /**
  * 解説ページ
- * 
+ *
  * @returns コンポーネント
- * 
+ *
  * @author nagisano33
  */
-export function ExplanationPage(){
-    return <>Explanation Page</>
+export function ExplanationPage() {
+  const { correctAnswer, explanation, isCorrect, sentence, usersAnswer } =
+    useExplanationPageQuery();
+
+  return (
+    <Stack gap={3}>
+      <UserResult
+        correctAnswer={correctAnswer}
+        isCorrect={isCorrect}
+        sentence={sentence}
+        usersAnswer={usersAnswer}
+      />
+      <ContentBox>
+        <Text fontSize={"2xl"} color={"orange"}>
+          解説
+        </Text>
+        <Text>aaaaaaaa</Text>
+      </ContentBox>
+      <Box textAlign={"right"}>
+        <Button colorScheme="blue">次の問題へ</Button>
+      </Box>
+    </Stack>
+  );
 }
